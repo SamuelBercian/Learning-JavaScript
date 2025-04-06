8a160a82423065bcbfa5a39b83397da8620eb887
Samuel Bercian <samuelbercian1706@gmail.com>
2025-04-06 00:55:43 -0600

    actualizacion bucles

---
+++ Semana-1-JS/bucles.js
@@ -0,0 +1,5 @@
+// Bucles: for
+
+for(let i = 1; i <= 20; i++){
+    console.log('Hola ', i);
+}
--- Semana-1-JS/tiposDeDatos.js
+++ Semana-1-JS/tiposDeDatos.js
@@ -1,9 +1,12 @@
 // Tipos de datos: numericos
 {
     let edad = 20; // Tipo entero (integer)
-    let dinero = 10.10 // Tipo decimal (float o numbes con putno decimal)
-    let infinito = Infinity // Tipo especial que representa el infinito
-    let noNumero = NaN // No es numero, representa un valor que no es numerico, aunque siga siendo un 'number'
+    let dinero = 10.10; // Tipo decimal (float o numbes con putno decimal)
+    let infinito = Infinity; // Tipo especial que representa el infinito
+    let noNumero = NaN; // No es numero, representa un valor que no es numerico, aunque siga siendo un 'number'
+    let resultado = null; // Valor nulo
+    let noDefinido = undefined // Una variable que no se le ha asignado un valor
+    
 }
 
 // Tipos de datos: cadenas
