aa114261c635219c0bfb7d213ec75d54d87701d9
Samuel Bercian <samuelbercian1706@gmail.com>
2025-04-08 13:45:28 -0600

    Parámetros y valores de retorno Semana 2 JS

---
+++ Semana-2-JS/parametrosYvaloresDeRetorno.js
@@ -0,0 +1,19 @@
+// Parametros y valores de retorno: Párametros
+// Son variables que se definen entre paréntesis al declarar una función. 
+// Sirven para recibir valores que la función utilizará internamente.
+function Ejemplo_1(nombre){
+    console.log('Hola, mi nombre es: ', nombre);
+}
+
+// nombre es el parámetro y "Samuel" es el argumento que se pasa al llamar la función.
+Ejemplo_1('Samuel');
+
+// Puede tener varios parámetros
+function Ejemplo1_2(num1, num2){
+    console.log('Suma: ', num1, '+', num2, '=', num1+num2);
+}
+Ejemplo1_2(2, 5);
+
+// Párametros y valores de retorno: Valores de Retorno
+// Una función puede devolver un valor usando la palabra clave return. 
+// Este valor puede ser almacenado o usado más adelante.
