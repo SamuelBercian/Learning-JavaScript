acc9681eb28440cc229d2744c7e5e035540fa9e1
Samuel Bercian <samuelbercian1706@gmail.com>
2025-04-08 21:28:42 -0600

    Modulo arrays completado

--- Semana-1-JS/tiposDeDatos.js
+++ Semana-1-JS/tiposDeDatos.js
@@ -1,7 +1,7 @@
 // Tipos de datos: numericos
 {
     let edad = 20; // Tipo entero (integer)
-    let dinero = 10.10; // Tipo decimal (float o numbes con putno decimal)
+    let dinero = 10.10; // Tipo decimal (float o numbes con punto decimal)
     let infinito = Infinity; // Tipo especial que representa el infinito
     let noNumero = NaN; // No es numero, representa un valor que no es numerico, aunque siga siendo un 'number'
     let resultado = null; // Valor nulo
--- Semana-2-JS/arrays.js
+++ Semana-2-JS/arrays.js
@@ -14,3 +14,51 @@ const Ejemplo2_2 = new Array('Array', 'Como', 'Constructor');
 
 // Arrays: Acceder a los elementos
 const Ejemplo_3 = ['Posicion 0', 'Posicion 1', 'Posicion 2'];
+console.log(Ejemplo_3[0]);
+
+// Para ver el tamaño del array (Basicamente la cantidad de elementos que tiene el array)
+console.log(Ejemplo_3.length);
+
+// Arrays: Modificacion de elementos
+// Puedes cambiar el valor de un elemento accediendo por su índice
+const Ejemplo_4 = [1, 2, 3, 4, 5];
+Ejemplo_4[2] = 6;
+
+console.log(Ejemplo_4[2]);
+
+// Arrays: Agregar elementos
+const Ejemplo_5 = ['Elemento 1', 'Elemento 2', 'Elemento 3'];
+// Agregar elemento al final
+Ejemplo_5.push('Elemento nuevo al final');
+// Agregar elemento al inicio
+Ejemplo_5.unshift('Elemento nuevo inicio');
+
+// Mostrara todo los elementos incluyendo los que se agregaron al principio y al final
+console.log(Ejemplo_5);
+
+// Arrays: Eliminar elementos
+const Ejemplo_6 = ['Elemento inicio', 'Elemento mitad', 'Elemento final'];
+// Eliminar ultimo elemento
+Ejemplo_6.pop();
+// Eliminar el primer elemento
+Ejemplo_6.shift();
+
+// Se mostrara el 'Elemento mitad' porque los otros estaran eliminados
+console.log(Ejemplo_6);
+
+// Arrays: Modificar o eliminar con splice
+const Ejemplo_7 = ['Elemento 1' /*Posicion 0*/, 'Elemento 2' /*Posicion 1*/, 'Elemento 3' /*Posicion 2*/, 'Elemento 4' /*Posicion 4*/];
+// Eliminar primer elemento
+// En Ejemplo_7.splice(1 es la posicion de donde se eliminara el elemento, 1 es la cantidad de elementos
+// que queremos eliminar), entonces (1 Posicion, 1 Cantidad a eliminar)
+// Se eliminara 'Elemento 1'
+Ejemplo_7.splice(0, 1);
+console.log(Ejemplo_7);
+// Se modificara sin eliminar
+// Ejemplo_7.splice(0 posicion, cantidad a modificar DEJAR EN 0, 'Nuevo elemento 1' sustituira
+// a 'Elemento 1'), entonces 'Elemento 1' pasara a llamarse 'Nuevo elemento 1'
+Ejemplo_7.splice(0, 0, 'Nuevo elemento 1');
+// Modificar 'Elemento 3'
+// En este caso Ejemplo_7.splice(2 posicion, 1 cantidad a afectar, 'Nuevo elemento 3')
+Ejemplo_7.splice(2, 1, 'Nuevo elemento 3');
+console.log(Ejemplo_7);
