9334dfab183d478cb1137d717de9ec4761359abd
Samuel Bercian <samuelbercian1706@gmail.com>
2025-07-21 17:22:39 -0600

    JavaScript Nivel 1

---
+++ JavaScript Nivel 1/Semana-1/Tema_1_Variables.js
@@ -0,0 +1,25 @@
+// VARIABLES EN JAVASCRIPT
+
+// VARIABLES EN JAVASCRIPT: VARIABLE VAR
+function variableVar()
+{
+    var nombre;
+    // Esta variable esta disponible en toda la funcion
+    // Su uso no es recomendable
+} 
+
+// VARIABLES EN JAVASCRIPT: VARIABLE LET
+function variableLet()
+{
+    let nombre = "Samuel";
+    // Uso especificamente para valores que pueden variar
+    // Esta variable esta disponible en el bloque "{}"
+}
+
+// VARIABLES EN JAVASCRIPT: VARIABLE CONST
+function variableConst()
+{
+    const pi = 3.1416;
+    // Uso especificamente para valores que no van a cambiar
+    // Esta variable esta disponible en el bloque "{}"
+}
---
+++ JavaScript Nivel 1/Semana-1/Tema_2_TiposDeDatos.js
@@ -0,0 +1,42 @@
+// TIPOS DE DATOS EN JAVASCRIPT
+
+// TIPOS DE DATOS EN JAVASCRIPT: DATOS PRIMITIVOS NUMBER
+function tipoDeDatosNumber()
+{
+    let numeros = 1;
+    // Esta variable es tipo number
+}
+
+// TIPOS DE DATOS EN JAVASCRIPT: DATOS PRIMITIVOS String
+function tipoDeDatosString()
+{
+    let texto = "String";
+    // Esta variable es de tipo String
+}
+
+// TIPOS DE DATOS EN JAVASCRIPT: DATOS PRIMITIVOS Boolean
+function tiposDeDatosBoolean()
+{
+    let boolean = true;
+    // Esta variable es Booleano
+}
+
+// TIPOS DE DATOS EN JAVASCRIPT: DATOS PRIMITIVOS Null
+function tipoDeDatosNull()
+{
+    let nulo = null;
+    // Esta variable es de tipo null (Sin valor)
+}
+
+// TIPOS DE DATOS EN JAVASCRIPT: DATOS PRIMITIVOS undefined
+function tiposDeDatosUndefined()
+{
+    let noDefinido = undefined;
+    // Esta variable esta sin definir
+}
+
+// TIPOS DE DATOS EN JAVASCRIPT: DATOS PRIMITIVOS Symbol
+function tiposDeDatosSymbol()
+{
+    // Esta variable es avanzada
+}
